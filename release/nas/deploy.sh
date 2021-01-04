a2edbde795a2fc3f03207ad4fa1ef68f98df91d9
Martin Joly <snwfdhmp@gmail.com>
2021-01-04 03:05:15 +0100

    docker-compose: add --build

--- release/nas/deploy.sh
+++ release/nas/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-/usr/bin/git pull origin master --ff-only && /usr/local/bin/docker-compose -f release/nas/docker-compose.yml up -d
+/usr/bin/git pull origin master --ff-only && /usr/local/bin/docker-compose -f release/nas/docker-compose.yml up --build -d
